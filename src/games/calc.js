b4a86185632bebc9ba02ef93f1742f2c4268b673
Sinner93 <chepukhin.sergey@gmail.com>
2020-02-25 23:00:39 +0500

    delet duplicate func of random count

--- src/games/calc.js
+++ src/games/calc.js
@@ -9,20 +9,12 @@ const max = 10;
 // Game "Calculate"
 const gameDescription = 'What is the result of the expression?".';
 
-// Function to get sample from array.
-const sample = (array) => {
-  const startNum = 0.1;
-  const lastNum = 0.9;
-  const len = array == null ? 0 : array.length;
-  return len ? array[Math.floor(getRandom(startNum, lastNum) * len)] : undefined;
-};
-
 const operators = ['+', '-', '*'];
 
 const generateData = () => {
   const randomCount1 = getRandom(min, max);
   const randomCount2 = getRandom(min, max);
-  const randomOperator = sample(operators);
+  const randomOperator = operators[getRandom(0, 2)];
 
   const question = `${randomCount1} ${randomOperator} ${randomCount2}`;
   let rightAnswer;

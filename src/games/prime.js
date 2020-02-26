ac3125998cdfae04baf92dac9401c273289f5270
Sinner93 <chepukhin.sergey@gmail.com>
2020-02-26 12:54:51 +0500

    move special function from 'generateData'

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,22 +7,22 @@ const min = 1;
 const max = 25;
 
 // Game "Prime"
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const generateData = () => {
-  const isPrime = (number) => {
-    if (number < 2) {
-      return false;
-    }
-
-    for (let i = 2; i < number / 2; i += 1) {
-      if (number % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
-
   const question = getRandom(min, max);
   const rightAnswer = isPrime(question) ? 'yes' : 'no';
 
